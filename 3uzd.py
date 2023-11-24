53e6d00c49dbe3f986610adffa2eb479f6680a7d
ralfsvilcins <ralfs.v06@gmail.com>
2023-11-24 13:57:13 +0200

    3 uzd

--- 3uzd.py
+++ 3uzd.py
@@ -1,8 +1,9 @@
-# Ievadiet skaitli no lietotāja
-ievaditais_skaitlis = int(input("Ievadiet skaitli: "))
+# Ievadiet skaitli 
+skaitlis = int(input("Ievadiet skaitli: "))
 
 # Pārbaude, vai skaitlis ir nepāra
-if ievaditais_skaitlis % 2 != 0:
-    print(ievaditais_skaitlis, "ir nepāra skaitlis.")
+if skaitlis % 2 != 0:
+    print(skaitlis, "ir nepāra skaitlis.")
 else:
-    print(ievaditais_skaitlis, "nav nepāra skaitlis.")
+    print(skaitlis, "nav nepāra skaitlis.")
+
